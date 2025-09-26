421a9fc6388e757fc176eb99999e420175c30cdf
previllage <previllagemunyadziwa@gmail.com>
2025-09-26 21:38:05 +0000

    Submition website Part 2

---
+++ J SCRIPT/JS sript.js
@@ -0,0 +1,9 @@
+//Toggle hamburger menu
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector("nav ul");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
+});
